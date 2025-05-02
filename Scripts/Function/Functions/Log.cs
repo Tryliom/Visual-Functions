185f19c15908422460d7de10a693ad5065eb4c9b
Alexis Haldy <alexishaldy@gmail.com>
2025-05-02 10:58:56 +0200

    Rework Log function

--- Scripts/Function/Functions/Log.cs
+++ Scripts/Function/Functions/Log.cs
@@ -2,29 +2,90 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum LogLevel
+{
+    Info,
+    Warning,
+    Error
+}
+
 namespace TryliomFunctions
 {
     [Serializable]
     public class Log : Function
     {
         public static readonly string Name = "Log";
-        public static readonly string Description = "Log any input value";
+        public static readonly string Description = "Log a message to the console using the formula string.\n" +
+                                                    "You need to encapsulate the string in double quotes.\n" +
+                                                    "Example: \"My variable is \" + myVariable + \" !\"";
         public static readonly FunctionCategory Category = FunctionCategory.Debug;
+        
+        public LogLevel LogLevel = LogLevel.Info;
+        
+        private List<ExpressionVariable> _variables = new();
 
 #if UNITY_EDITOR
         public override void GenerateFields()
         {
-            AllowAddInput(new FunctionSettings("ObjectsToLog"));
+            Inputs.Add(new Field("Formula", typeof(Formula)).AllowAnyMethod());
+            EditableAttributes.Add(nameof(LogLevel));
+            
+            AllowAddInput(new FunctionSettings().AllowMethods());
         }
 #endif
 
         protected override bool Process(List<Field> variables)
         {
-            var logString = string.Empty;
+            if (_variables.Count != variables.Count + Inputs.Count)
+            {
+                _variables.Clear();
+                _variables.Capacity = variables.Count + Inputs.Count;
+                
+                foreach (var field in variables)
+                {
+                    _variables.Add(new ExpressionVariable(field.FieldName, field.Value));
+                }
+                
+                foreach (var field in Inputs)
+                {
+                    _variables.Add(new ExpressionVariable(field.FieldName, field.Value));
+                }
+            }
+            else
+            {
+                for (var i = 0; i < variables.Count; i++)
+                {
+                    _variables[i].Name = variables[i].FieldName;
+                    _variables[i].Value = variables[i].Value;
+                }
 
-            foreach (var field in Inputs) logString += field.Value.Value + " ";
+                for (var i = variables.Count; i < Inputs.Count; i++)
+                {
+                    _variables[i].Name = Inputs[i].FieldName;
+                    _variables[i].Value = Inputs[i].Value;
+                }
+            }
+            
+            var formula = (string) Inputs[0].Value.Value;
+            var results = Evaluator.Process(Uid, formula, _variables);
 
-            Debug.Log(logString);
+            foreach (var result in results)
+            {
+                if (result is not string str) continue;
+                
+                switch (LogLevel)
+                {
+                    case LogLevel.Info:
+                        Debug.Log(str);
+                        break;
+                    case LogLevel.Warning:
+                        Debug.LogWarning(str);
+                        break;
+                    case LogLevel.Error:
+                        Debug.LogError(str);
+                        break;
+                }
+            }
 
             return true;
         }
