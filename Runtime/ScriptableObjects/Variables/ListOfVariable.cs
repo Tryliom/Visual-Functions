a39dabe1b181a7e6c9fb342476579651c77d775f
Tryliom <alexishaldy@gmail.com>
2025-07-07 13:04:02 +0200

    Add a smoother way to get a list from a ListOf class

--- Runtime/ScriptableObjects/Variables/ListOfVariable.cs
+++ Runtime/ScriptableObjects/Variables/ListOfVariable.cs
@@ -1,9 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace VisualFunctions
 {
     [CreateAssetMenu(menuName = "Visual Functions/Variables/ListOf")]
     public class ListOfVariable : Variable<ListOf>
     {
+        /**
+         * Returns the list of values as a List[TType]
+         */
+        public List<TType> GetList<TType>()
+        {
+            return Value.ListValue.Value as List<TType>;
+        }
     }
 }
